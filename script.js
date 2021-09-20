08555d0756601018cc47581dad6ce0cd0529bd0e
Jalfaro1010 <Joshua.alfaro@gmail.com>
2021-09-20 15:35:33 -0400

    jsbasics complete exercise

---
+++ script.js
@@ -0,0 +1,27 @@
+// Log To Create Favorite Number
+console.log(99+98);
+
+// Primitives
+// Numbers, Strings, Booleans, NUll, Undefined
+
+
+// Operator Symbols:
+// "+", "-", "*", "/", "**, "%""
+
+// Modulus Remainder of 1 from 5
+//("1%5" 5 goes into 1 0 times with a remainder of 1) 
+console.log(1%5);
+// (5 goes into 11 2 times with a remainder of 1)
+console.log(11%5);
+
+// I'M MARY POPPINS, Y'ALL! display in the terminal/console
+console.log('"I\'M MARY POPPINS, Y\'ALL!"');
+
+// Concatenation
+console.log("Hawk " + "Eye");
+
+//String for First and Last Name Length Property
+console.log("Joshua Alfaro".length); //13
+
+// Index to select lowercase "e" at the end of the string
+console.log("Avengers: Endgame"[16]);
